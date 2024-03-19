30666457d9373a74b7e10c0c3192729574a6890f
DaveWestFGCU <dawest6674@eagle.fgcu.edu>
2024-03-19 09:12:51 -0400

    Fixed post-season type 'SuperBowl' from 'Superbowl'

--- webscraper/football_cfg.py
+++ webscraper/football_cfg.py
@@ -3,7 +3,7 @@ regular_season_range = (2023, 2023)
 regular_season_weeks = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18']
 
 postseason_range = (1966, 2023)     # Superbowl I in 1967 (1966 season)
-postseason_game_types = ['WildCard', 'Division', 'ConfChamp', 'Superbowl', 'Champ']
+postseason_game_types = ['WildCard', 'Division', 'ConfChamp', 'SuperBowl', 'Champ']
 
 offense_positions = ['QB', 'RB', 'FB', 'TB', 'HB', 'OL', 'G', 'LG', 'RG', 'T', 'LT', 'RT', 'C', 'WR', 'TE', 'WR-RB']
 defense_positions = ['DL', 'DE', 'LE', 'RE', 'DT', 'NT', 'LB', 'MLB', 'ILB', 'OLB', 'LOLB', 'ROLB', 'SLB', 'WLB', 'DB', 'CB', 'S', 'SS', 'FS', 'EDGE', 'RILB']
